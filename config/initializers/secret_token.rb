aa6913f699ef7593aca251526f77c69caae81834
Darren Cox <darren@audioandpixels.com>
2014-02-13 19:28:22 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audioandpixels::Application.config.secret_key_base = '4874eb6e5236de14c3852c25e82f6e0d6045950a5eca4854b6195421926dc5d13e33acb553cf48d4dfd1ac2d68bfa461a3da868d4ad472c7d1c679b62bebb4e3'
